b5c5c6dbf311fac66207356f5bc4ce2a1269cfff
Vitaly Kargin <kargin@kodeks.ru>
2022-07-31 15:10:58 +0300

    zabbix minor fix

--- zabbix/install/zabbix-server-install.sh
+++ zabbix/install/zabbix-server-install.sh
@@ -20,7 +20,6 @@ sudo chmod 700 /data
 UNIT='postgresql-14.service'
 DIR="/etc/systemd/system/${UNIT}.d"
 sudo mkdir $DIR
-# echo -e "[Service]\nEnvironment=PGDATA=/data/pgsql/14/data" > ${DIR}/override.conf
 echo -e "[Service]\nEnvironment=PGDATA=/data/pgsql/14/data" | sudo tee -a ${DIR}/override.conf
 sudo systemctl daemon-reload
 
@@ -34,7 +33,7 @@ sudo -u postgres psql -c "show data_directory;"
 
 sudo yum install -y yum-utils cmake gcc git python-devel postgresql14-devel openssl-devel krb5-devel redhat-rpm-config
 
-sudo find / -name "pg_config"
+find / -name "pg_config" 2>/dev/null
 export PATH=$PATH:/usr/pgsql-14/bin
 
 cd /tmp
@@ -52,7 +51,6 @@ sudo systemctl restart postgresql-14
 sudo rpm -Uvh https://repo.zabbix.com/zabbix/6.2/rhel/9/x86_64/zabbix-release-6.2-2.el9.noarch.rpm
 sudo yum install -y zabbix-server-pgsql zabbix-web-pgsql zabbix-nginx-conf zabbix-sql-scripts zabbix-selinux-policy zabbix-agent
 
-# sudo -u postgres createuser -P zabbix
 sudo -u postgres createuser $DBUser
 sudo -u postgres psql -c "ALTER USER $DBUser WITH PASSWORD '$DBPassword';"
 sudo -u postgres createdb -O $DBUser $DBName
@@ -60,24 +58,18 @@ echo "CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;" | sudo -u postgres ps
 echo "\dx;" | sudo -u postgres psql $DBUser
 
 cd /usr/share/doc/zabbix-sql-scripts/postgresql/
-zcat server.sql.gz | sudo -u zabbix psql zabbix
-cat timescaledb.sql | sudo -u zabbix psql zabbix
+zcat server.sql.gz | sudo -u $DBUser psql $DBUser
+cat timescaledb.sql | sudo -u $DBUser psql $DBUser
 
 # echo "host    zabbix          zabbix          127.0.0.1/32            md5" >> /data/pgsql/14/data/pg_hba.conf
-echo "host    zabbix          zabbix          127.0.0.1/32            md5" | sudo tee -a /data/pgsql/14/data/pg_hba.conf
+echo "host    $DBUser          $DBUser          127.0.0.1/32            md5" | sudo tee -a /data/pgsql/14/data/pg_hba.conf
 sudo systemctl restart postgresql-14
 
-DBHost=127.0.0.1
-DBName=zabbix
-DBUser=zabbix
-DBPassword=JX62fh83fBj4*dwk
 sudo sed -i "s/# DBHost=localhost/DBHost=$DBHost/g" /etc/zabbix/zabbix_server.conf
 sudo sed -i "s/DBName=zabbix/DBName=$DBName/g" /etc/zabbix/zabbix_server.conf
 sudo sed -i "s/DBUser=zabbix/DBUser=$DBUser/g" /etc/zabbix/zabbix_server.conf
 sudo sed -i "s/# DBPassword=/DBPassword=$DBPassword/g" /etc/zabbix/zabbix_server.conf
 
-Port=80
-FQDN=zabbix.corp.lan
 sudo sed -i "s/#        listen          8080;/        listen          $Port;/g" /etc/nginx/conf.d/zabbix.conf
 sudo sed -i "s/#        server_name     example.com;/        server_name     $FQDN;/g" /etc/nginx/conf.d/zabbix.conf
 
